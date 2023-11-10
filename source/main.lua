bc1d5cce4a5fe4c254d4b92dbaefcd6539a1f4ab
Ryan <ryanmaddennew@gmail.com>
2023-11-10 10:52:53 -0500

    squid god sprite tutorial

--- source/main.lua
+++ source/main.lua
@@ -1,16 +1,24 @@
+import "CoreLibs/object"
 import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+
+import "circle"
+import "spike"
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
-local playerX, playerY = 200,  120
-local playerRadius = 10
-local playerSpeed = 6
+local function initialize()
+    local circleSprite = Circle(100, 80, 20)
+    circleSprite:add()
+    local circleSprite2 = Circle(100, 160, 15)
+    circleSprite2:add()
+    local spike = Spike(250, 120)
+    spike:add()
+end
 
-function pd.update()
-    gfx.clear()
-    local crankAngle = math.rad(pd.getCrankPosition())
-    playerX += math.sin(crankAngle) * playerSpeed
-    playerY -= math.cos(crankAngle) * playerSpeed
-    gfx.fillCircleAtPoint(playerX, playerY, playerRadius)
+initialize()
+
+function playdate.update()
+    gfx.sprite.update()
 end
